c3f0633e1f3dac5b4f641835c947eb5a79f45a23
Vitaliy <viurum986@mail.ru>
2022-04-04 21:57:02 +0300

    init  Task 0208

---
+++ Task 0208/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов
+
+void FillArray(int[,] array)
+{
+        for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+
+
+        }
+        Console.WriteLine();
+    }
+}
+
+int Sum(int[,] array)
+{
+    int sum = 0;
+    int minSum = 0;
+    int minNum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == 0) 
+            {
+                sum += array[i, j];
+                minSum += array[i, j]; 
+            }
+            else sum += array[i, j]; 
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            minNum = i;
+        }
+        sum = 0;
+    }
+    return minNum;
+}
+int[,] array = new int[4, 5];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Cтрока с наименьшей суммой элементов: " + Sum(array));
