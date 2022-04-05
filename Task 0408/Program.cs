50e9f58f42c15a7893eb786ca4122139efd20b0e
Vitaliy <viurum986@mail.ru>
2022-04-05 23:56:37 +0300

    init  Task 0408

---
+++ Task 0408/Program.cs
@@ -0,0 +1,70 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+
+void FillArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+
+    void PrintArray(int[,,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int k = 0; k < array.GetLength(2); k++)
+                {
+                    Console.Write($"{array[i, j, k]}\t");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine("");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine("");
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}\t");
+
+            }
+            Console.WriteLine("");
+        }
+
+    }
+
+
+    {
+        int[,,] elemet = array;
+        for (int i = 0; i < elemet.GetLength(0); i++)
+        {
+            for (int j = 0; j < elemet.GetLength(1); j++)
+            {
+                Console.WriteLine("");
+                for (int k = 0; k < elemet.GetLength(2); k++)
+
+                    Console.Write(" Индекс элементов " +  i +  ": "  +  array[i, j, k]);
+
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
+int[,,] array = new int[3, 3, 3];
+
+FillArray(array);
+
+
+
